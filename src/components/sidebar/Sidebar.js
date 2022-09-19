c8ae8c6584bbe6fd22c6fdfbfa48bba184d24933
tassiane anzolin <tassianeanzolin@alunos.utfpr.edu.br>
2022-09-19 20:23:52 -0300

    🛠 components pt 2

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,10 +1,23 @@
 import './Sidebar.css';
 import logo from '../../assets/logo.svg';
 
-const Sidebar = ({ openSidebar, closeSidebar}) => {
-    return(
-        <div className={openSidebar ? sidebar-responsive : ""}>
-
+const Sidebar = ({ openSidebar, closeSidebar }) => {
+    return (
+        <div className={openSidebar ? 'sidebar-responsive' : ''} id='sidebar'>
+            <div className='sidebar__title'>
+                <div className='sidebar__img'>
+                    <img src={logo} alt='logo'></img>
+                    <h1>Teste</h1>
+                </div>
+                <i onClick={() => closeSidebar()}
+                    className='fa fa-times'
+                    id='sidebarIcon'
+                    aria-hidden='true'
+                ></i>
+            </div>
+            <div className='sidebar__menu'>
+                <div className='sidebar__link'></div>
+            </div>
         </div>
     )
 
