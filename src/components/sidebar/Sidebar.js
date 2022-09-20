c1e36b85a17ec5a2b234bdf02a9242cbd8a2708b
tassiane anzolin <tassianeanzolin@alunos.utfpr.edu.br>
2022-09-19 22:58:27 -0300

    🛠 components pt 3 and ✨ sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -7,7 +7,7 @@ const Sidebar = ({ sidebarOpen, closeSidebar }) => {
             <div className='sidebar__title'>
                 <div className='sidebar__img'>
                     <img src={logo} alt='logo'></img>
-                    <h1>Teste</h1>
+                    <h1>Dashboard</h1>
                 </div>
 
                 <i onClick={() => closeSidebar()}
@@ -20,29 +20,37 @@ const Sidebar = ({ sidebarOpen, closeSidebar }) => {
             <div className='sidebar__menu'>
 
                 <div className='sidebar__link active_menu_link'>
-                    <i className='fa fa-minus-square'></i>
+                    <i className='fa fa-house'></i>
                     <a href='#'>Home</a>
                 </div>
 
                 <h2>Admin</h2>
                 <div className='sidebar__link'>
                     <i className='fa fa-tachometer'></i>
-                    <a href='#'>Área admininstrativa</a>
+                    <a href='#'>Painel Geral</a>
                 </div>
                 <div className='sidebar__link'>
                     <i className='fa fa-archive'></i>
                     <a href='#'>Relatórios</a>
                 </div>
                 <div className='sidebar__link'>
-                    <i className='fa fa-building'></i>
+                    <i className='fa fa-industry'></i>
+                    <a href='#'>Esteiras</a>
+                </div>
+                <div className='sidebar__link'>
+                    <i className='fa fa-truck'></i>
                     <a href='#'>Distribuidores</a>
                 </div>
                 <div className='sidebar__link'>
-                    <i className='fa fa-bars'></i>
+                    <i className='fa fa-gear'></i>
+                    <a href='#'>Configurações</a>
+                </div>
+                <div className='sidebar__link'>
+                    <i className='fa fa-headset'></i>
                     <a href='#'>Suporte</a>
                 </div>
                 <div className='sidebar__logout'>
-                    <i className='fa fa-power-off'></i>
+                    <i className='fa fa-right-from-bracket'></i>
                     <a href='#'>Log out</a>
                 </div>
 
